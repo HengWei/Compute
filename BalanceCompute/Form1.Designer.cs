138383ec874ce3e5907ff01c779c52afaa23ec63
Henry <swatduck@gmail.com>
2024-02-19 10:56:45 +0800

    新增_分錄範本

--- BalanceCompute/Form1.Designer.cs
+++ BalanceCompute/Form1.Designer.cs
@@ -37,14 +37,22 @@
             button2 = new Button();
             textBox2 = new TextBox();
             label2 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            button5 = new Button();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(25, 31);
+            label1.Location = new Point(22, 42);
             label1.Name = "label1";
-            label1.Size = new Size(67, 15);
+            label1.Size = new Size(72, 15);
             label1.TabIndex = 0;
             label1.Text = "匯入明細檔";
             label1.Click += label1_Click;
@@ -55,14 +63,14 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(98, 31);
+            textBox1.Location = new Point(95, 42);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(358, 23);
+            textBox1.Size = new Size(454, 23);
             textBox1.TabIndex = 3;
             // 
             // button1
             // 
-            button1.Location = new Point(480, 31);
+            button1.Location = new Point(568, 42);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 5;
@@ -72,25 +80,25 @@
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(33, 140);
+            textBox3.Location = new Point(24, 397);
             textBox3.Multiline = true;
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(423, 258);
+            textBox3.Size = new Size(667, 258);
             textBox3.TabIndex = 7;
             // 
             // button3
             // 
-            button3.Location = new Point(480, 140);
+            button3.Location = new Point(568, 135);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Size = new Size(75, 58);
             button3.TabIndex = 8;
-            button3.Text = "開始處理";
+            button3.Text = "產生交易總表";
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(480, 87);
+            button2.Location = new Point(568, 98);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 11;
@@ -100,35 +108,99 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(98, 87);
+            textBox2.Location = new Point(95, 98);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(358, 23);
+            textBox2.Size = new Size(454, 23);
             textBox2.TabIndex = 10;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(25, 87);
+            label2.Location = new Point(22, 98);
             label2.Name = "label2";
-            label2.Size = new Size(55, 15);
+            label2.Size = new Size(59, 15);
             label2.TabIndex = 9;
             label2.Text = "銀行交易";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Location = new Point(24, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(667, 199);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "交易總表";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button5);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Location = new Point(24, 227);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(667, 155);
+            groupBox2.TabIndex = 13;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "分錄產出";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(568, 87);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 46);
+            button5.TabIndex = 9;
+            button5.Text = "產生分錄";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 45);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 15);
+            label3.TabIndex = 6;
+            label3.Text = "匯入交易總表";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(113, 42);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(436, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(568, 41);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 8;
+            button4.Text = "匯入";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(589, 423);
-            Controls.Add(button2);
-            Controls.Add(textBox2);
-            Controls.Add(label2);
-            Controls.Add(button3);
+            ClientSize = new Size(703, 686);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(textBox3);
-            Controls.Add(button1);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
             Name = "Form1";
             Text = "總表產生工具 v 1.1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -144,5 +216,11 @@
         private Button button2;
         private TextBox textBox2;
         private Label label2;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label3;
+        private TextBox textBox4;
+        private Button button4;
+        private Button button5;
     }
 }
--- BalanceCompute/Form1.cs
+++ BalanceCompute/Form1.cs
@@ -1,6 +1,7 @@
 using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Vml.Spreadsheet;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace BalanceCompute
 {
@@ -77,7 +78,7 @@ namespace BalanceCompute
             }
 
 
-            
+
         }
 
 
@@ -217,10 +218,10 @@ namespace BalanceCompute
 
             decimal[] feeRate = new decimal[] { 1.8m, 1.5m, 1.5m, 2.2m };
 
-            
+
 
             string fileName = AppDomain.CurrentDomain.BaseDirectory + String.Format("{0}-{1}.xlsx"
-                , rawDatas.OrderBy(x=>x.Date).FirstOrDefault().Date.ToString("MMdd")
+                , rawDatas.OrderBy(x => x.Date).FirstOrDefault().Date.ToString("MMdd")
                 , rawDatas.OrderByDescending(x => x.Date).FirstOrDefault().Date.ToString("MMdd"));
 
             using (var wb = new XLWorkbook())
@@ -256,7 +257,7 @@ namespace BalanceCompute
                 }
 
 
-                
+
 
                 foreach (var date in rawDatas.GroupBy(x => x.Date).OrderBy(x => x.Key))
                 {
@@ -324,16 +325,15 @@ namespace BalanceCompute
                                 break;
                         }
 
-                        if(tranData.Count()>0)
+                        if (tranData.Count() > 0)
                         {
                             ws.Cell(row, ++col).SetValue(tranData.Count() > 0 ? tranData.FirstOrDefault().Date.ToString("MM/dd") : string.Empty);
                             ws.Cell(row, ++col).SetValue(tranData.Sum(x => x.Amount));
 
-                            if(tranData.Sum(x => x.Amount)!= total)
+                            if (tranData.Sum(x => x.Amount) != total)
                             {
                                 ws.Cell(row, col).Style.Fill.BackgroundColor = XLColor.Red;
                             }
-
                         }
                         else
                         {
@@ -342,9 +342,6 @@ namespace BalanceCompute
                     }
                 }
 
-
-                //ws.Columns().AdjustToContents();
-
                 ws.SheetView.Freeze(2, 1);
 
                 wb.SaveAs(fileName);
@@ -352,175 +349,6 @@ namespace BalanceCompute
 
             return fileName;
 
-        }
-
-
-        public static IEnumerable<SystemData> LoadSysytemData(string filePath, out DateTime? date, out string message)
-        {
-            message = string.Empty;
-
-            date = null;
-
-            List<SystemData> data = new List<SystemData>();
-
-            using (var wb = new XLWorkbook(filePath))
-            {
-                var ws = wb.Worksheet("第 1 頁");
-
-                var lastRow = ws.LastRowUsed().RowNumber();
-
-                for (int i = 1; i <= lastRow; i++)
-                {
-                    string rowData = ws.Cell(i, 1).Value.ToString() ?? string.Empty;
-
-                    if (rowData.IndexOf("門市代號") > -1)
-                    {
-                        SystemData temp = new SystemData();
-
-                        var idxSpec = rowData.LastIndexOf(" ");
-
-                        temp.Store = rowData.Substring(idxSpec, rowData.Length - idxSpec).Trim().Replace("圓環", "店");
-
-                        i += 3;
-
-                        decimal.TryParse(ws.Cell(i, 2).Value.ToString() ?? string.Empty, out decimal amount);
-
-                        temp.Cash = amount;
-
-                        if (!date.HasValue)
-                        {
-                            var dateArray = (ws.Cell(i, 1).Value.ToString() ?? String.Empty).Split("/");
-
-                            date = new DateTime(int.Parse(dateArray[0]) + 1911, int.Parse(dateArray[1]), int.Parse(dateArray[2]));
-                        }
-
-                        data.Add(temp);
-                    }
-                }
-            }
-
-            if (data.Count() == 0)
-            {
-                message = "頂新資料空白";
-            }
-
-            return data;
-        }
-
-        private static IEnumerable<BalanceData> LoadBalanceData(string filePath, out string title, out string message)
-        {
-            List<BalanceData> data = new List<BalanceData>();
-
-            message = string.Empty;
-            title = string.Empty;
-
-            using (var wb = new XLWorkbook(filePath))
-            {
-                var ws = wb.Worksheet(1);
-
-                var lastRow = ws.LastRowUsed().RowNumber();
-
-                int j = 0;
-
-                while (true)
-                {
-                    var tempTitle = ws.Cell(1, ++j).Value.ToString() ?? string.Empty;
-
-                    if (string.IsNullOrEmpty(tempTitle))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        title = tempTitle;
-                    }
-                }
-
-
-                for (int i = 2; i <= lastRow; i++)
-                {
-                    string rowData = ws.Cell(i, 1).Value.ToString() ?? string.Empty;
-
-                    BalanceData temp = new BalanceData();
-
-
-                    temp.Store = rowData;
-
-                    if (temp.Store.IndexOf("合計") > -1)
-                    {
-                        break;
-                    }
-
-                    decimal.TryParse(ws.Cell(i, 2).Value.ToString() ?? string.Empty, out decimal amount);
-
-                    temp.LastBalance = amount;
-
-                    data.Add(temp);
-                }
-            }
-
-            return data;
-        }
-
-        private static string ExportResult(IEnumerable<BalanceData> _Balance, IEnumerable<SystemData> _System, string title, DateTime date)
-        {
-            string fileName = AppDomain.CurrentDomain.BaseDirectory + String.Format("{0}.xlsx", date.ToString("MMdd"));
-
-            using (var wb = new XLWorkbook())
-            {
-                var ws = wb.AddWorksheet(date.ToString("MMdd"));
-
-                int i = 1;
-
-                int j = 1;
-
-                ws.Cell(i, j++).SetValue("門市");
-                ws.Cell(i, j++).SetValue(title);
-                ws.Cell(i, j++).SetValue(string.Format("{0}現金收入", date.ToString("MM/dd")));
-                ws.Cell(i, j++).SetValue(string.Format("{0}餘額", date.ToString("MM/dd")));
-
-
-                foreach (var item in _Balance)
-                {
-                    var system = _System.FirstOrDefault(x => x.Store.IndexOf(item.Store) > -1);
-
-                    item.Cash = system.Cash;
-
-                    j = 1;
-
-                    ws.Cell(++i, j).SetValue(item.Store);
-
-                    ws.Cell(i, ++j).SetValue(item.LastBalance);
-                    ws.Cell(i, j).Style.NumberFormat.Format = "#,##0";
-
-                    ws.Cell(i, ++j).SetValue(item.Cash);
-                    ws.Cell(i, j).Style.NumberFormat.Format = "#,##0";
-
-                    ws.Cell(i, ++j).SetValue(item.NowBalance);
-                    ws.Cell(i, j).Style.NumberFormat.Format = "#,##0";
-                }
-
-
-                j = 1;
-                ws.Cell(++i, j).SetValue("合計");
-                ws.Range(i, 1, i, 4).Style.Border.TopBorder = XLBorderStyleValues.Thin;
-
-                ws.Cell(i, ++j).SetValue(_Balance.Sum(x => x.LastBalance));
-                ws.Cell(i, j).Style.NumberFormat.Format = "#,##0";
-
-                ws.Cell(i, ++j).SetValue(_Balance.Sum(x => x.Cash));
-                ws.Cell(i, j).Style.NumberFormat.Format = "#,##0";
-
-                ws.Cell(i, ++j).SetValue(_Balance.Sum(x => x.NowBalance));
-                ws.Cell(i, j).Style.NumberFormat.Format = "#,##0";
-
-
-                ws.Columns().AdjustToContents();
-
-                wb.SaveAs(fileName);
-            }
-
-            return fileName;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -538,5 +366,156 @@ namespace BalanceCompute
                 textBox2.Text = openFileDialog1.FileName;
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                DefaultExt = "xlsx",
+                Filter = "Excel File (*.xlsx)|*.xlsx"
+            };
+
+            var fileResult = openFileDialog1.ShowDialog();
+
+            if (fileResult == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox3.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            string message = string.Empty;
+
+            var path = GenerSystemFile(out message);
+
+
+            if (string.IsNullOrEmpty(message) == false)
+            {
+                textBox3.Text = textBox3.Text + Environment.NewLine + message;
+            }
+            else
+            {
+                textBox3.Text = textBox3.Text + Environment.NewLine + $"完成 路徑: {path}";
+            }
+
+
+        }
+
+
+        public static string GenerSystemFile(out string message)
+        {
+            message = string.Empty;
+
+
+            string[] department = new string[] { "總部大樓", "資策會", "國票" };
+            string[] serialNo = new string[] { "00000389", "00000390", "00000505" };
+
+            string[] payment = new string[] { "銀行信用卡", "悠遊卡", "一卡通", "LINE PAY" };
+            int[] paymentDay = new int[] { 0, 1, 2, 7 };
+
+            int[] colorR = new int[] { 255, 197, 252, 178 };
+            int[] colorG = new int[] { 255, 217, 213, 222 };
+            int[] colorB = new int[] { 0, 241, 180, 130 };
+
+
+            string[] title1 = new string[] { "BUKRS", "BLART", "BLDAT", "BUDAT", "MONAT", "BKTXT", "WAERS", "LDGRP", "KURSF_EXT", "WWERT", "XBLNR", "PARGB_HDR", "XMWST" };
+            string[] title2 = new string[] { "*公司代碼 (4)", "*日記帳分錄類型 (2)", "*日記帳分錄日期", "*過帳日期", "會計期間 (2)", "文件表頭內文 (25)", "*交易幣別 (5)", "分類帳群組 (4)", "匯率 (12)", "幣別換算日期", "參考文件號碼 (16)", "夥伴業務範圍 (4)", "自動計算稅金 (1)" };
+
+            string[] detailTitel1 = new string[] { "BUKRS", "HKONT", "SGTXT", "WRSOL", "WRHAB", "DMBTR", "DMBE2", "MWSKZ", "TXJCD", "KOSTL", "PRCTR", "AUFNR", "PS_POSID", "VALUT", "HBKID", "HKTID", "ZUONR", "VBUND", "SEGMENT" };
+            string[] detailTitel2 = new string[] { "公司代碼 (4)", "總帳科目 (10)", "項目內文 (50)", "借方", "貸方", "金額〈公司代碼幣別〉", "Amount in second local currency (LC2)"
+                , "稅碼 (2)", "租稅管轄權 (15)", "成本中心 (10)", "利潤中心 (10)", "訂單號碼 (12)", "WBS 元素 (24)", "起息日", "往來銀行 (5)", "往來銀行帳戶 (5)","指派號碼 (18)","貿易夥伴 (6)","區段報表製作的區段 (10)" };
+
+
+
+            decimal[] feeRate = new decimal[] { 1.8m, 1.5m, 1.5m, 2.2m };
+
+
+            //string dateFormat = String.Format("{0}-{1}"
+            //    , rawDatas.OrderBy(x => x.Date).FirstOrDefault().Date.ToString("MMdd")
+            //    , rawDatas.OrderByDescending(x => x.Date).FirstOrDefault().Date.ToString("MMdd"));
+
+            string dateFormat = "TEST";
+
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + $"分錄{dateFormat}.xlsx";
+
+            using (var wb = new XLWorkbook())
+            {
+                var ws = wb.AddWorksheet(dateFormat);
+
+                int row = 0;
+
+                int col = 0;
+
+
+                ws.Cell(++row, ++col).SetValue("上傳一般日記帳分錄");
+                ws.Cell(++row, col).SetValue("// To add field columns to the template, please add technical names.");
+                ws.Cell(row, col).Style.Alignment.WrapText = false;
+                ws.Cell(++row, col).SetValue("// For a complete list of field columns and their technical names, choose ?in the right upper corner of the app screen and then view the Browseentry of web assistance.\r\n");
+                ws.Cell(row, col).Style.Alignment.WrapText = false;              
+                ws.Cell(++row, col).SetValue("批次 ID");
+                ws.Range(row, col, row, col + 1).Style.Fill.BackgroundColor = XLColor.FromArgb(255, 192, 0);
+                
+
+                for (int i = 0; i < 2; i++)
+                {
+                    ws.Cell(row += 3, 1).SetValue(i+1);
+                    ws.Cell(row , 2).SetValue("表頭");
+                    ws.Range(row, 1, row, 20).Style.Fill.BackgroundColor = XLColor.FromArgb(142, 169, 219);
+
+                    col = 1;
+                    //8
+
+                    ++row;
+                    col = 1;
+                    for (int k = 0; k < title1.Length; k++)
+                    {
+                        ws.Cell(row, ++col).SetValue(title1[k]);
+
+                        ws.Cell(row+1, col).SetValue(title2[k]);
+
+                    }
+
+                    ws.Range(row, 2, row, title1.Length+1).Style.Fill.BackgroundColor = XLColor.FromArgb(237, 241, 249);
+                    ws.Range(row+1, 2, row+1, title1.Length + 1).Style.Fill.BackgroundColor = XLColor.FromArgb(237, 241, 249);
+
+
+                    //資料列
+                    row+=2;
+
+                    //空一行
+                    ++row;
+                    ws.Cell(++row, 2).SetValue("明細項目");
+                    ws.Cell(row, 2).Style.Fill.BackgroundColor = XLColor.FromArgb(142, 169, 219);
+
+                    ++row;
+                    ws.Range(row, 5, row, 6).Merge();
+                    ws.Cell(row, 5).SetValue("交易幣別");
+                    ws.Cell(row, 5).Style.Fill.BackgroundColor = XLColor.FromArgb(237, 241, 249);
+
+                    row++;
+                    col = 1;
+                    for (int k = 0; k < detailTitel1.Length; k++)
+                    {
+                        ws.Cell(row, ++col).SetValue(detailTitel1[k]);
+
+                        ws.Cell(row + 1, col).SetValue(detailTitel2[k]);
+                    }
+
+                    ws.Range(row, 2, row, detailTitel1.Length + 1).Style.Fill.BackgroundColor = XLColor.FromArgb(237, 241, 249);
+                    ws.Range(row + 1, 2, row + 1, detailTitel1.Length + 1).Style.Fill.BackgroundColor = XLColor.FromArgb(237, 241, 249);
+
+                    //資料列
+
+                }                
+
+                wb.SaveAs(fileName);
+            }
+
+            return fileName;
+
+        }
+
     }
 }
